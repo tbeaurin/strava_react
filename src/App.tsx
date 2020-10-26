2adac8c716d8120cc99fbbbcc4e2efd22c1fa1a8
Tristan Beaurin <tristan.beaurin@gmail.com>
2020-10-26 15:08:55 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button href="#">
+          Se connecter avec Strava
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
