b4ebf17a1fce98c02731c2bebed2c274ed446c06
Tristan Beaurin <tristan.beaurin@gmail.com>
2020-10-27 11:28:37 +0100

    connexion page

--- src/Ui/AppScene.test.tsx
+++ src/Ui/AppScene.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AppScene from './AppScene';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<AppScene />);
+  const linkElement = screen.getByText(/se connecter/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import orange from '@material-ui/core/colors/orange';
+import green from '@material-ui/core/colors/green';
+import AppScene from './Ui/AppScene';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: orange,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <AppScene />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
